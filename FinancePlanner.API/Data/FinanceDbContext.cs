239becf0c5170448f306e79640e48d1fc52ec61a
sharma <sharma@DESKTOP-IS2799H>
2025-04-12 13:56:32 +0530

    Add project files.

---
+++ FinancePlanner.API/Data/FinanceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinancePlanner.API.Models;
+
+namespace FinancePlanner.API.Data
+{
+    public class FinanceDbContext : DbContext
+    {
+        public FinanceDbContext(DbContextOptions<FinanceDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
